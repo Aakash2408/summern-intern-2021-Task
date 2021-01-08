8a46b448bb6945d447d065af980d6df487d6ce78
aakash2408 <aakashsangwan024@gmail.com>
2021-01-08 21:05:52 +0530

    search bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
+import SearchBar from "./components/Search/Searchbar";
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const changeSearchValue = (searchResult) => {
+    setSearch(searchResult);
+  };
   return (
     <div className="App">
        <Header text={"The Shoppies"} />
       <main className="container parent" data-test="component-app">
        
           <Banner text={"You have nominated five movies!"} />
-       
+           
+          <div className="div1">
+          <SearchBar changeSearchValue={changeSearchValue} />
+        </div>
         </main>
     </div>
   );
---
+++ src/components/Search/Searchbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Searchbar.scss";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+function SearchBar({ changeSearchValue }) {
+  return (
+    <form className="search-form">
+<div class="container_box">
+      <input type="text" id="box"  type="text"
+        id="movie"
+        name="movie" placeholder="Search any Movie" class="search__box"  onChange={(event) => changeSearchValue(event.target.value)}/>
+        <FontAwesomeIcon icon={faSearch} className="search__icon" />
+  </div>
+
+    </form>
+  );
+}
+
+export default SearchBar;
